c83ace82a00e739563da0e56772b8a11c892da94
TheCaptain989 <thecaptain989@protonmail.com>
2024-06-17 13:47:22 -0500

    Test of tags

--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@
 # shellcheck disable=SC2015
 
 # check if curl and jq are installed
-if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null; then
+if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null || ! command -v xmllint &>/dev/null; then
     sudo apt-get update
-    sudo apt-get install curl jq -y
+    sudo apt-get install curl jq xmllint -y
 fi
 
 # clean pkg.txt
@@ -25,6 +25,7 @@ while IFS= read -r line; do
     owner=$(echo "$line" | cut -d'/' -f1)
     repo=$(echo "$line" | cut -d'/' -f2)
     image=$(echo "$line" | cut -d'/' -f3)
+    tag=$(echo "$line" | cut -d'/' -f4)
     [ -f index.json ] || echo "[]" >index.json
 
     # manual update: skip if the package is already in the index; the rest are updated on a consistent basis
@@ -33,12 +34,25 @@ while IFS= read -r line; do
             any(.[]; .owner == $owner and .repo == $repo and .image == $image)' index.json >/dev/null && continue || :
     fi
 
-    # get the number of pulls, skipping nans
+    if [ -n "$tag" ]; then
+      pages=$(curl -sSLN https://github.com/"$owner"/"$repo"/pkgs/container/"$image"/versions | grep -Pzo '(?<=<em class="current" data-total-pages=")\d*')
+      [ -z "$pages" ] && pages=1
+      for i in $(seq 1 "$pages"); do
+         curl -sSLN https://github.com/"$owner"/"$repo"/pkgs/container/"$image"/versions?page="$i" >page.html
+         pulls=$(xmllint --html --recover --xpath "//a[text()=\"$tag\"]/../../../div[2]/span/text()" page.html 2>/dev/null | tr -d '\f\n ')
+         raw_pulls=$(echo "$pulls" | tr -d ',')
+         [ -n "$pulls" ] && break
+      done
+      rm page.html
+    else
+        # get the number of pulls, skipping nans
+        html=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image")
+        raw_pulls=$(echo -e "$html" | grep -Pzo '(?<=Total downloads</span>\n          <h3 title=")\d*')
+        [[ "$raw_pulls" =~ ^[0-9]+$ ]] || continue
+        pulls=$(echo "$raw_pulls" | awk '{ split("k M B T P E Z Y", v); s=0; while( $1>999.9 ) { $1/=1000; s++ } print int($1*10)/10 v[s] }') || pulls=-1
+    fi
     date=$(date -u +"%Y-%m-%d")
-    html=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image")
-    raw_pulls=$(echo -e "$html" | grep -Pzo '(?<=Total downloads</span>\n          <h3 title=")\d*')
-    [[ "$raw_pulls" =~ ^[0-9]+$ ]] || continue
-    pulls=$(echo "$raw_pulls" | awk '{ split("k M B T P E Z Y", v); s=0; while( $1>999.9 ) { $1/=1000; s++ } print int($1*10)/10 v[s] }') || pulls=-1
+
     jq --arg owner "$owner" --arg repo "$repo" --arg image "$image" --arg pulls "$pulls" --arg raw_pulls "$raw_pulls" --arg date "$date" '
         if . == [] then
             [{owner: $owner, repo: $repo, image: $image, pulls: $pulls, raw_pulls: $raw_pulls, raw_pulls_all: {($date): $raw_pulls}}]
@@ -65,16 +79,17 @@ for i in $(jq -r '.[] | @base64' index.json); do
     owner=$(_jq '.owner')
     repo=$(_jq '.repo')
     image=$(_jq '.image')
+    tag=$(_jq '.tag')
     pulls=$(_jq '.pulls')
     raw_pulls=$(_jq '.raw_pulls')
-    export owner repo image
-    printf "%s (%s) %s/%s/%s\n" "$pulls" "$raw_pulls" "$owner" "$repo" "$image"
+    export owner repo image tag
 
     # ...that have not been added yet
-    grep -q "$owner/$repo/$image" README.md || perl -0777 -pe '
+    grep -q "$owner/$repo/$image/$tag" README.md || perl -0777 -pe '
     my $owner = $ENV{"owner"};
     my $repo = $ENV{"repo"};
     my $image = $ENV{"image"};
+    my $tag = $ENV{"tag"};
 
     # decode percent-encoded characters
     for ($owner, $repo, $image) {
@@ -84,6 +99,7 @@ for i in $(jq -r '.[] | @base64' index.json); do
     $label =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
 
     # add new badge
-    s/\n\n(\[!\[.*)\n\n/\n\n$1 \[!\[$owner\/$repo\/$image\]\(https:\/\/img.shields.io\/badge\/dynamic\/json\?url=https%3A%2F%2Fraw.githubusercontent.com%2Fipitio%2Fghcr-pulls%2Fmaster%2Findex.json\&query=%24%5B%3F(%40.owner%3D%3D%22$owner%22%20%26%26%20%40.repo%3D%3D%22$repo%22%20%26%26%20%40.image%3D%3D%22$image%22)%5D.pulls\&label=$label\)\]\(https:\/\/github.com\/$owner\/$repo\/pkgs\/container\/$image\)\n\n/g;
-' README.md >README.tmp && [ -f README.tmp ] && mv README.tmp README.md || :
+    s/\n\n(\[!\[.*)\n\n/\n\n$1 \[!\[$owner\/$repo\/$image\/$tag\]\(https:\/\/img.shields.io\/badge\/dynamic\/json\?url=https%3A%2F%2Fraw.githubusercontent.com%2Fthecaptain989%2Fghcr-pulls%2Fmaster%2Findex.json\&query=%24%5B%3F(%40.owner%3D%3D%22$owner%22%20%26%26%20%40.repo%3D%3D%22$repo%22%20%26%26%20%40.image%3D%3D%22$image%22%22%20%26%26%20%40.tag%3D%3D%22$tag%22)%5D.pulls\&label=$image\/$tag\)\]\(https:\/\/github.com\/$owner\/$repo\/pkgs\/container\/$image\)\n\n/g;
+' README.md > README.tmp && [ -f README.tmp ] && mv README.tmp README.md || :
+    fi
 done
