04c04f1e60c37e336ecefcd27349db105069a8ac
TheCaptain989 <thecaptain989@protonmail.com>
2025-03-10 14:56:09 +0000

    Cleaned up code for readability

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Update the number of pulls for each package in pkg.txt
 # Usage: ./update.sh
-# Dependencies: curl, jq
+# Dependencies: curl, jq, xmllint
 # From: ipitio, TheCaptain989
 #
 # shellcheck disable=SC2015
 
 error_count=0
+temp_file=index.tmp.json
 
-# check if curl, jq, and xmllint are installed
+# Check if curl, jq, and xmllint are installed
 echo "::group::Checking for and installing dependencies"
 if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null || ! command -v xmllint &>/dev/null; then
     sudo apt-get update
@@ -16,57 +17,85 @@ if ! command -v curl &>/dev/null || ! command -v jq &>/dev/null || ! command -v 
 fi
 echo "::endgroup::"
 
-# Prepare GitHub acctions summary
+# Prepare GitHub actions summary
 echo "### Pull Counts" >> $GITHUB_STEP_SUMMARY
 echo "| Repository | Package| Tag | Pulls | Date |" >> $GITHUB_STEP_SUMMARY
 echo "| --- | --- | --- | --- | --- |" >> $GITHUB_STEP_SUMMARY
 
-# update the index with new counts
+# Update the index with new counts
 while IFS= read -r line; do
     owner=$(echo "$line" | cut -d'/' -f1)
     repo=$(echo "$line" | cut -d'/' -f2)
     image=$(echo "$line" | cut -d'/' -f3)
     tag=$(echo "$line" | cut -d'/' -f4)
     json_file="./docs/$tag.json"
-    [ -f "$json_file" ] || echo "{}" >"$json_file" # create the index if it does not exist
+    # Create the index if it does not exist
+    [ -f "$json_file" ] || echo "{}" >"$json_file"
     unset raw_pulls
 
-    # use xmllint and walk through version pages if querying tags
+    # Use xmllint and walk through version pages if querying tags
     if [ -n "$tag" ]; then
+        # Get the number of version pages
         pages=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image/versions" | grep -Po '(?<=data-total-pages=")\d*')
         [ -z "$pages" ] && pages=1
-        # Testing if the error prints correctly
-        printf "\n::error title=Unknown Tag::Could not find tag %s in %s/%s/%s\n" "$tag" "$owner" "$repo" "$image"
+        # Display status (ASCII escape code for blue)
         printf "\u001b[34mCrawling $pages pages of $owner/$repo/$image for tag $tag : "
         for i in $(seq 1 "$pages"); do
-            raw_pulls=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image/versions?page=$i" | xmllint --html --recover --xpath "//a[text()=\"$tag\"]/../../../div[2]/span/text()" - 2>/dev/null | tr -d '\f\n, ')
+            # Print the page number
             printf "$i"
-            [ -n "$raw_pulls" ] && break
-            [ "$i" -ne "$pages" ] && printf "," || { printf "\n::error title=Unknown Tag::Could not find tag %s in %s/%s/%s\n" "$tag" "$owner" "$repo" "$image"; printf "*ERROR:* Could not find tag %s in %s/%s/%s\n" "$tag" "$owner" "$repo" "$image" >> $GITHUB_STEP_SUMMARY; ((error_count++)); continue 2; }
+            # Get the number of pulls
+            raw_pulls=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image/versions?page=$i" | xmllint --html --recover --xpath "//a[text()=\"$tag\"]/../../../div[2]/span/text()" - 2>/dev/null | tr -d '\f\n, ')
+            [ -n "$raw_pulls" ] && { printf "\n"; break; }
+            # Error if last page
+            if [ "$i" -eq "$pages" ]; then
+                # Display error message if the tag is not found
+                printf "\n::error title=Missing Tag::Could not find tag %s in %s/%s/%s\n" "$tag" "$owner" "$repo" "$image"
+                # Annotation already displays error message in the summary
+                # printf "> [!CAUTION]\n> Could not find tag %s in %s/%s/%s\n" "$tag" "$owner" "$repo" "$image" >> $GITHUB_STEP_SUMMARY
+                ((error_count++))
+                continue 2
+            fi
+            printf ","
         done
-        printf "\n"
     else
-        # get the number of pulls,
+        # Get the number of pulls if not querying tags
         raw_pulls=$(curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image" | grep -Po '(?<=Total downloads</span>\n          <h3 title=")\d*')
         curl -sSLNZ "https://github.com/$owner/$repo/pkgs/container/$image" >temp.html
     fi
-    [ -z "$raw_pulls" ] && { printf "::error title=No Pull Counts::No raw pull counts found for %s/%s/%s\n" "$owner" "$repo" "$image"; printf "*ERROR:* No raw pull counts found for %s/%s/%s\n" "$owner" "$repo" "$image" >> $GITHUB_STEP_SUMMARY; ((error_count++)); continue; }
+    # Display an error if the raw_pulls is empty
+    if [ -z "$raw_pulls" ]; then
+        printf "::error title=No Pull Counts::No raw pull counts found for %s/%s/%s\n" "$owner" "$repo" "$image"
+        # Annotation already displays error message in the summary
+        # printf "> [!CAUTION]\n> No raw pull counts found for %s/%s/%s\n" "$owner" "$repo" "$image" >> $GITHUB_STEP_SUMMARY
+        ((error_count++))
+        continue
+    fi
+    # Format the number of pulls
     pulls=$(numfmt --to si --round nearest --format "%f" "$raw_pulls")
     date=$(date -u +"%Y-%m-%d")
+    # Display the pull counts
     printf "%s/%s/%s/%s = %s (%s) %s\n" "$owner" "$repo" "$image" "$tag" "$raw_pulls" "$pulls" "$date"
     printf "| %s/%s | %s | %s | %s (%s) | %s |\n" "$owner" "$repo" "$image" "$tag" "$raw_pulls" "$pulls" "$date" >> $GITHUB_STEP_SUMMARY
 
+    # Update the index file
     jq --arg owner "$owner" --arg repo "$repo" --arg image "$image" --arg tag "$tag" --arg pulls "$pulls" --arg raw_pulls "$raw_pulls" --arg date "$date" '
         if . == {} then
             {owner: $owner, repo: $repo, image: $image, tag: $tag, pulls: $pulls, raw_pulls: $raw_pulls, raw_pulls_all: {($date): $raw_pulls}}
         else
             .pulls = $pulls | .raw_pulls = $raw_pulls | .raw_pulls_all[($date)] = $raw_pulls
-        end' "$json_file" >index.tmp.json
-    [ -s index.tmp.json ] && mv index.tmp.json "$json_file" || { printf "ERROR: Empty JSON file. Exiting.\n"; printf "*ERROR:* Empty JSON file. Exiting.\n" >> $GITHUB_STEP_SUMMARY; exit 1; }
+        end' "$json_file" >"$temp_file"
+    # Display an error if the temporary index file is empty
+    if [ ! -s "$temp_file" ]; then
+        printf "::error title=Empty JSON::Empty JSON file. Exiting.\n"
+        # Annotation already displays error message in the summary
+        # printf "> [!CAUTION]\n> Empty JSON file. Exiting.\n" >> $GITHUB_STEP_SUMMARY
+        ((error_count++))
+        continue
+    fi
+    mv "$temp_file" "$json_file"
 done <pkg.txt
 
-# # sort the index by the number of raw_pulls greatest to smallest
-# jq 'sort_by(.raw_pulls | tonumber) | reverse' "$json_file" >index.tmp.json
-# mv index.tmp.json "$json_file"
+# Ensure temp file is gone before commit
+[ -f "$temp_file" ] && rm "$temp_file"
 
 exit $error_count
